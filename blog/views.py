887910b90ac1f3e2f16d87d16e476c96773f9fc8
Preston Ezell <pezell2@ivytech.edu>
2023-10-08 17:52:50 -0400

    added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -12,6 +12,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
